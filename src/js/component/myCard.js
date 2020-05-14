496237361922def5d9c673c279583b202a3826a6
Tozzigh <tozzigh@gmail.com>
2020-05-14 13:02:59 +0000

    Ejercisio

---
+++ src/js/component/myCard.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function MyCard(texto) {
+	return (
+		<div className="col-lg-3 col-md-6 mb-4">
+			<div className="card h-100">
+				<img
+					className="card-img-top"
+					src="http://placehold.it/500x325"
+					alt=""
+				/>
+				<div className="card-body">
+					<h4 className="card-title">Card title</h4>
+					<p className="card-text">{texto}</p>
+				</div>
+				<div className="card-footer">
+					<a href="#" className="btn btn-primary">
+						Find Out More!
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-
+import PropTypes from "prop-types";
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
@@ -9,7 +9,30 @@ import "bootstrap";
 import "../styles/index.scss";
 
 //import your own components
-import { Home } from "./component/home.js";
-
+import { MyCard } from "./component/myCard";
+import { Navbar } from "./component/navbar";
+import { Jumbotron } from "./component/jumbotron";
+import { Footer } from "./component/footer";
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+const arrayContenido = [
+	"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
+	"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto.",
+	"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente esse necessitatibus neque.",
+	"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Explicabo magni sapiente, tempore debitis beatae culpa natus architecto."
+];
+const arrayCards = arrayContenido.map(testo => MyCard(testo));
+MyCard.propTypes = {
+	testo: PropTypes.string
+};
+
+const cardCards = <div className="row text-center">{arrayCards}</div>;
+const cardContainer = (
+	<div className="container">{[Jumbotron(), cardCards]}</div>
+);
+
+ReactDOM.render(
+	<div>
+		{Navbar()}, {cardContainer}, {Footer()}
+	</div>,
+	document.body
+);
